3a43a6de2f558a306e945cef57091bfeb986c1af
Mate Antunovic <just.mate.antunovic@gmail.com>
2018-08-27 16:35:28 +1200

    Add skeleton files

---
+++ command_op.py
@@ -0,0 +1,15 @@
+class CommandOp:
+    def __init__(self, command, params):
+        pass
+
+    def check_ready(self):
+        pass
+
+    def run(self):
+        pass 
+
+    def was_success(self):
+        pass
+
+    def handle_error(self):
+        pass
---
+++ create_dirs.py
@@ -0,0 +1,15 @@
+class CommandOp:
+    def __init__(self, dirs):
+        pass
+
+    def check_ready(self):
+        pass
+
+    def run(self):
+        pass 
+
+    def was_success(self):
+        pass
+
+    def handle_error(self):
+        pass
